b9d0f64a830a7126bb33a4ebb22d1566baf77756
cyyber <kaushal.forex@gmail.com>
2018-05-27 16:58:57 +0530

    Replaced all hardcoded constants with config

--- misc/ntp.go
+++ misc/ntp.go
@@ -4,34 +4,37 @@ import (
 	"github.com/beevik/ntp"
 	"time"
 	"sync"
+	"github.com/cyyber/go-qrl/core"
 )
 
 type NTP struct {
 	lock *sync.Mutex
+	
 	drift uint64
 	lastUpdate uint64
+	config *core.Config
 }
 
 func (n *NTP) UpdateTime() error {
 	n.lock.Lock()
 	defer n.lock.Unlock()
 
-	NTP_RETRIES := 6
-
 	var err error
 	var t time.Time
 
-	for retry := 0; retry <= NTP_RETRIES; retry++ {
-		t, err = ntp.Time("pool.ntp.org")
+	for retry := 0; retry <= n.config.User.NTP.Retries; retry++ {
+		for _, server := range n.config.User.NTP.Servers {
+			t, err = ntp.Time(server)
 
-		if err != nil {
-			continue
+			if err != nil {
+				continue
+			}
+
+			n.drift = uint64(time.Now().Second() - t.Second())
+			n.lastUpdate = uint64(t.Second())
+
+			return nil
 		}
-
-		n.drift = uint64(time.Now().Second() - t.Second())
-		n.lastUpdate = uint64(t.Second())
-
-		return nil
 	}
 
 	return err
@@ -39,7 +42,7 @@ func (n *NTP) UpdateTime() error {
 
 func (n *NTP) Time() (uint64, error) {
 	currentTime := uint64(time.Now().Second()) + n.drift
-	if currentTime - n.lastUpdate > 12 * 60 * 60 {
+	if currentTime - n.lastUpdate > n.config.User.NTP.Refresh {
 		err := n.UpdateTime()
 		return 0, err
 	}
