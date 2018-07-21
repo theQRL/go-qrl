d8c3bb68c45e5552310633fe74834c3b5c6077eb
cyyber <kaushal.forex@gmail.com>
2018-07-21 20:02:40 +0530

    Implemented PoWValidator

---
+++ pow/powvalidator.go
@@ -0,0 +1,35 @@
+package pow
+
+import (
+	"sync"
+	"github.com/theQRL/qryptonight/goqryptonight"
+	"github.com/cyyber/go-qrl/misc"
+)
+
+type PoWValidatorInterface interface {
+	VerifyInput([]byte, []byte) bool
+
+	// TODO: Add Cache version for VerifyInput
+}
+
+type PoWValidator struct {
+	lock sync.Mutex
+
+	ph goqryptonight.PoWHelper
+}
+
+func (p *PoWValidator) VerifyInput(miningBlob []byte, target []byte) bool {
+	return p.ph.VerifyInput(misc.BytesToUCharVector(miningBlob), misc.BytesToUCharVector(target))
+}
+
+var once sync.Once
+var pv *PoWValidator
+
+func GetPowValidator() *PoWValidator {
+	once.Do(func() {
+		pv = &PoWValidator{}
+		pv.ph = goqryptonight.NewPoWHelper()
+	})
+
+	return pv
+}
