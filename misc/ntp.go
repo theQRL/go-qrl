220197f82bf52e214a79f4c6e5710805d43e8799
cyyber <kaushal.forex@gmail.com>
2018-07-21 20:03:05 +0530

    Implemented Singleton for NTP

--- misc/ntp.go
+++ misc/ntp.go
@@ -9,7 +9,7 @@ import (
 
 type NTP struct {
 	lock *sync.Mutex
-	
+
 	drift uint64
 	lastUpdate uint64
 	config *core.Config
@@ -40,20 +40,25 @@ func (n *NTP) UpdateTime() error {
 	return err
 }
 
-func (n *NTP) Time() (uint64, error) {
+func (n *NTP) Time() uint64 {
 	currentTime := uint64(time.Now().Second()) + n.drift
 	if currentTime - n.lastUpdate > n.config.User.NTP.Refresh {
 		err := n.UpdateTime()
-		return 0, err
+		if err != nil {
+			// TODO: log warning here
+		}
 	}
 
-	return uint64(time.Now().Second()) + n.drift, nil
+	return uint64(time.Now().Second()) + n.drift
 }
 
-func CreateNTP() (*NTP, error) {
-	n := &NTP{}
+var once sync.Once
+var n *NTP
 
-	err := n.UpdateTime()
+func GetNTP() *NTP {
+	once.Do(func() {
+		n = &NTP{}
+	})
 
-	return n, err
+	return n
 }
