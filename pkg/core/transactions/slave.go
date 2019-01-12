d1d26a58cb2fce6ad52d8a660c9730f6272cea4b
cyyber <kaushal.forex@gmail.com>
2019-01-12 21:02:01 +0530

    Added PlainSlaveTransaction for JSON representation of SlaveTransaction

--- pkg/core/transactions/slave.go
+++ pkg/core/transactions/slave.go
@@ -13,6 +13,32 @@ import (
 	"github.com/theQRL/qrllib/goqrllib/goqrllib"
 )
 
+type PlainSlaveTransaction struct {
+	MasterAddress string  `json:"master_address"`
+	Fee uint64  `json:"fee"`
+	PublicKey string  `json:"public_key"`
+	Signature string  `json:"signature"`
+	Nonce uint64  `json:"nonce"`
+	TransactionHash string  `json:"transaction_hash"`
+	TransactionType string  `json:"transaction_type"`
+
+	SlavePKs []string  `json:"slave_public_keys"`
+	AccessTypes []uint32  `json:"access_types"`
+}
+
+func (t *PlainSlaveTransaction) TransactionFromPBData(tx *generated.Transaction) {
+	t.Fee = tx.Fee
+	t.PublicKey = misc.Bin2HStr(tx.PublicKey)
+	t.Signature = misc.Bin2HStr(tx.Signature)
+	t.Nonce = tx.Nonce
+	t.TransactionHash = misc.Bin2HStr(tx.TransactionHash)
+	t.TransactionType = "slave"
+
+	slave := tx.GetSlave()
+	t.SlavePKs = misc.Bin2Pks(slave.SlavePks)
+	t.AccessTypes = slave.AccessTypes
+}
+
 type SlaveTransaction struct {
 	Transaction
 }
