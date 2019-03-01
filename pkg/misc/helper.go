eb5012227c46506ebafd6ffcbc82646e911ba1f9
cyyber <kaushal.forex@gmail.com>
2019-03-01 22:19:14 +0530

    Added BytesToString helper function

--- pkg/misc/helper.go
+++ pkg/misc/helper.go
@@ -182,3 +182,10 @@ func ConvertBytesToLong(b []byte) uint32 {
 func OTSKeyFromSig(signature []byte) uint64 {
 	return uint64(binary.BigEndian.Uint32(signature[0:4]))
 }
+
+func BytesToString(data []byte) (string, error) {
+	buff := bytes.NewBuffer(data)
+	var result string
+	_, err := buff.WriteString(result)
+	return result, err
+}
